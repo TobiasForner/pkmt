5eba9f633204be3a1cf05a440b427e847137ce25
Tobias <t.forner@tum.de>
2024-08-18 16:51:30 +0200

    refactor

--- src/document_component.rs
+++ src/document_component.rs
@@ -1,4 +1,12 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{bail, Context, Result};
+
+use crate::obsidian_parsing::parse_obsidian;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum DocumentElement {
@@ -114,6 +122,88 @@ impl DocumentComponent {
     pub fn new_text(text: &str) -> Self {
         Self::new(DocumentElement::Text(text.to_string()))
     }
+}
+
+fn apply_substitutions(text: &str) -> String {
+    text.replace('−', "-")
+        .replace('∗', "*")
+        .replace('∈', "\\in ")
+        .replace("“", "\"")
+        .replace("”", "\"")
+        .replace("∃", "EXISTS")
+        .replace("’", "'")
+        .replace("_", "_")
+        .replace("–", "-")
+}
+
+pub fn convert_tree(root_dir: PathBuf, target_dir: PathBuf, mode: &str) -> Result<()> {
+    let root_dir = root_dir.canonicalize()?;
+    let files = files_in_tree(&root_dir)?;
+    let _ = std::fs::create_dir_all(&target_dir)?;
+    let target_dir = target_dir.canonicalize()?;
+    println!("target: {target_dir:?}");
+
+    let _ = files
+        .iter()
+        .map(|f| {
+            let rel = pathdiff::diff_paths(&f, &root_dir).unwrap();
+            let target = target_dir.join(&rel);
+            convert_file(f, &target, &mode)
+        })
+        .collect::<Result<()>>()?;
+    Ok(())
+}
+
+pub fn convert_file<T: AsRef<Path> + Debug, U: AsRef<Path> + Debug>(
+    file: T,
+    out_file: U,
+    mode: &str,
+) -> Result<()> {
+    let file = file.as_ref();
+    let text = std::fs::read_to_string(&file)?;
+    let text = apply_substitutions(&text);
+    let res = match mode {
+        "Obsidian" => parse_obsidian(&text),
+        _ => panic!("Unsupported mode: {mode}"),
+    };
+
+    if let Ok(components) = res {
+        let text = to_logseq_text(&components);
+
+        let res =
+            std::fs::write(&out_file, text).context(format!("Failed to write to {out_file:?}"));
+        if res.is_err() {
+            bail!("Encountered: {res:?}!");
+        }
+        Ok(())
+    } else {
+        bail!("Could not convert the file {file:?} to obsidian: {res:?}")
+    }
+}
+
+fn files_in_tree<T: AsRef<Path>>(root_dir: T) -> Result<Vec<PathBuf>> {
+    let mut res = vec![];
+    let root_dir = root_dir.as_ref().canonicalize()?;
+    let dir_entry = root_dir.read_dir()?;
+    let tmp: Result<()> = dir_entry
+        .into_iter()
+        .map(|f| {
+            let path = f.unwrap().path();
+            if path.is_dir() {
+                let rec = files_in_tree(&path)?;
+                res.extend(rec.into_iter());
+            } else if let Some(ext) = path.extension() {
+                if ["md"].contains(&ext.to_str().unwrap_or("should not be found")) {
+                    res.push(path.clone());
+                }
+            }
+            Ok(())
+        })
+        .collect();
+    if tmp.is_err() {
+        bail!("Encountered error: {tmp:?}!")
+    }
+    Ok(res)
 }
 
 pub fn to_logseq_text(components: &Vec<DocumentComponent>) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
-use document_component::to_logseq_text;
-use obsidian_parsing::parse_obsidian;
 mod file_checklist;
+use document_component::{convert_file, convert_tree};
 use file_checklist::checklist_for_tree;
 
-use std::{
-    fmt::Debug,
-    path::{Path, PathBuf},
-};
+use std::{fmt::Debug, path::PathBuf};
 mod document_component;
 
 mod obsidian_parsing;
@@ -89,86 +85,4 @@ fn run() -> Result<()> {
         None => panic!("Failed to parse arguments!"),
     };
     res
-}
-
-fn apply_substitutions(text: &str) -> String {
-    text.replace('−', "-")
-        .replace('∗', "*")
-        .replace('∈', "\\in ")
-        .replace("“", "\"")
-        .replace("”", "\"")
-        .replace("∃", "EXISTS")
-        .replace("’", "'")
-        .replace("_", "_")
-        .replace("–", "-")
-}
-
-fn convert_tree(root_dir: PathBuf, target_dir: PathBuf, mode: &str) -> Result<()> {
-    let root_dir = root_dir.canonicalize()?;
-    let files = files_in_tree(&root_dir)?;
-    let _ = std::fs::create_dir_all(&target_dir)?;
-    let target_dir = target_dir.canonicalize()?;
-    println!("target: {target_dir:?}");
-
-    let _ = files
-        .iter()
-        .map(|f| {
-            let rel = pathdiff::diff_paths(&f, &root_dir).unwrap();
-            let target = target_dir.join(&rel);
-            convert_file(f, &target, &mode)
-        })
-        .collect::<Result<()>>()?;
-    Ok(())
-}
-
-fn convert_file<T: AsRef<Path> + Debug, U: AsRef<Path> + Debug>(
-    file: T,
-    out_file: U,
-    mode: &str,
-) -> Result<()> {
-    let file = file.as_ref();
-    let text = std::fs::read_to_string(&file)?;
-    let text = apply_substitutions(&text);
-    let res = match mode {
-        "Obsidian" => parse_obsidian(&text),
-        _ => panic!("Unsupported mode: {mode}"),
-    };
-
-    if let Ok(components) = res {
-        let text = to_logseq_text(&components);
-
-        let res =
-            std::fs::write(&out_file, text).context(format!("Failed to write to {out_file:?}"));
-        if res.is_err() {
-            bail!("Encountered: {res:?}!");
-        }
-        Ok(())
-    } else {
-        bail!("Could not convert the file {file:?} to obsidian: {res:?}")
-    }
-}
-
-fn files_in_tree<T: AsRef<Path>>(root_dir: T) -> Result<Vec<PathBuf>> {
-    let mut res = vec![];
-    let root_dir = root_dir.as_ref().canonicalize()?;
-    let dir_entry = root_dir.read_dir()?;
-    let tmp: Result<()> = dir_entry
-        .into_iter()
-        .map(|f| {
-            let path = f.unwrap().path();
-            if path.is_dir() {
-                let rec = files_in_tree(&path)?;
-                res.extend(rec.into_iter());
-            } else if let Some(ext) = path.extension() {
-                if ["md"].contains(&ext.to_str().unwrap_or("should not be found")) {
-                    res.push(path.clone());
-                }
-            }
-            Ok(())
-        })
-        .collect();
-    if tmp.is_err() {
-        bail!("Encountered error: {tmp:?}!")
-    }
-    Ok(res)
 }
