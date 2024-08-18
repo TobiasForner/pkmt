46801607c75f1808298315d7d2ac59c6dbb68570
Tobias <t.forner@tum.de>
2024-08-18 16:46:06 +0200

    add checklist

---
+++ src/file_checklist.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use std::path::Path;
+
+pub fn checklist_for_tree<T: AsRef<Path>>(root_dir: T, todo_marker: &str) -> Result<String> {
+    let root_dir = root_dir.as_ref().canonicalize()?;
+    let dir_entry = root_dir.read_dir()?;
+    let mut files = vec![];
+    let mut dirs = vec![];
+    let _ = dir_entry
+        .into_iter()
+        .map(|f| {
+            let path = f.unwrap().path();
+            if path.is_dir() {
+                dirs.push(path);
+            } else if let Some(ext) = path.extension() {
+                if ["md"].contains(&ext.to_str().unwrap_or("should not be found")) {
+                    files.push(path);
+                }
+            }
+            Ok(())
+        })
+        .collect::<Result<()>>()?;
+
+    let mut lines = vec![format!("- {todo_marker} {}", root_dir.to_string_lossy())];
+    if !files.is_empty() {
+        lines.push(format!("\t- {todo_marker} files_in_dir"));
+        files
+            .iter()
+            .for_each(|f| lines.push(format!("\t\t- {todo_marker} {}", f.to_string_lossy())));
+    }
+    if !dirs.is_empty() {
+        let dir_text = dirs
+            .iter()
+            .map(|d| {
+                let rec = checklist_for_tree(d, todo_marker)?;
+                let rec: Vec<String> = rec.lines().map(|l| format!("\t{l}")).collect();
+                Ok(rec.join("\n"))
+            })
+            .collect::<Result<Vec<String>>>()?;
+        lines.extend(dir_text.into_iter());
+    }
+
+    Ok(lines.join("\n"))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,94 @@
 use anyhow::{bail, Context, Result};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use document_component::to_logseq_text;
 use obsidian_parsing::parse_obsidian;
+mod file_checklist;
+use file_checklist::checklist_for_tree;
 
-use std::path::{Path, PathBuf};
+use std::{
+    fmt::Debug,
+    path::{Path, PathBuf},
+};
 mod document_component;
 
 mod obsidian_parsing;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
-    /// file to parse
-    #[arg(short, long)]
-    file: PathBuf,
-
-    /// destination file to write to
-    #[arg(short, long)]
-    out_file: PathBuf,
-
-    ///parsing mode
-    #[arg(short, long)]
-    mode: Option<String>,
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// convert between different formats
+    Convert {
+        /// path to parse. If this is a directory, the out_path must also be a directory. Missing directories in out_path will be created.
+        #[arg(short, long)]
+        in_path: PathBuf,
+
+        /// destination path to write to
+        #[arg(short, long)]
+        out_path: PathBuf,
+
+        /// parsing mode
+        #[arg(short, long)]
+        mode: Option<String>,
+    },
+    /// generate a file checklist
+    Checklist {
+        /// root directory to generate the checklist for
+        #[arg(short, long)]
+        root_dir: PathBuf,
+
+        /// file to write the checklist to
+        #[arg(short, long)]
+        out_file: PathBuf,
+        /// String to use to signal a todo
+        #[arg(short, long)]
+        todo_marker: String,
+    },
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {}
+
 fn main() {
-    let args = Args::parse();
-    let file = args.file;
-    let out_file = args.out_file;
-    let mode = args.mode.unwrap_or("Obsidian".to_string());
-    let res = convert_tree(file, out_file, &mode);
+    let res = run();
     println!("{res:?}");
+}
+
+fn run() -> Result<()> {
+    let cli = Cli::parse();
+    let res: Result<()> = match cli.command {
+        Some(Commands::Checklist {
+            root_dir,
+            out_file,
+            todo_marker,
+        }) => {
+            let res = checklist_for_tree(root_dir, &todo_marker)?;
+            let _ = std::fs::write(&out_file, res)
+                .context(format!("Could not write checklist to {out_file:?}!"))?;
+            Ok(())
+        }
+        Some(Commands::Convert {
+            in_path,
+            out_path,
+            mode,
+        }) => {
+            let mode = mode.unwrap_or("Obsidian".to_string());
+            if in_path.is_dir() {
+                let _ = convert_tree(in_path, out_path, &mode)?;
+            } else {
+                let _ = convert_file(in_path, out_path, &mode)?;
+            }
+            Ok(())
+        }
+        None => panic!("Failed to parse arguments!"),
+    };
+    res
 }
 
 fn apply_substitutions(text: &str) -> String {
@@ -55,31 +113,39 @@ fn convert_tree(root_dir: PathBuf, target_dir: PathBuf, mode: &str) -> Result<()
     let _ = files
         .iter()
         .map(|f| {
-            let text = std::fs::read_to_string(&f)?;
-            let text = apply_substitutions(&text);
-            let res = match mode {
-                "Obsidian" => parse_obsidian(&text),
-                _ => panic!("Unsupported mode: {mode}"),
-            };
-
-            if let Ok(components) = res {
-                let text = to_logseq_text(&components);
-
-                let rel = pathdiff::diff_paths(&f, &root_dir).unwrap();
-                let target = target_dir.join(&rel);
-                println!("{f:?} --> {target:?} ({rel:?})");
-                let res =
-                    std::fs::write(&target, text).context(format!("Failed to write to {target:?}"));
-                if res.is_err() {
-                    bail!("Encountered: {res:?}!");
-                }
-                Ok(())
-            } else {
-                bail!("Could not convert the file {f:?} to obsidian: {res:?}")
-            }
+            let rel = pathdiff::diff_paths(&f, &root_dir).unwrap();
+            let target = target_dir.join(&rel);
+            convert_file(f, &target, &mode)
         })
         .collect::<Result<()>>()?;
     Ok(())
+}
+
+fn convert_file<T: AsRef<Path> + Debug, U: AsRef<Path> + Debug>(
+    file: T,
+    out_file: U,
+    mode: &str,
+) -> Result<()> {
+    let file = file.as_ref();
+    let text = std::fs::read_to_string(&file)?;
+    let text = apply_substitutions(&text);
+    let res = match mode {
+        "Obsidian" => parse_obsidian(&text),
+        _ => panic!("Unsupported mode: {mode}"),
+    };
+
+    if let Ok(components) = res {
+        let text = to_logseq_text(&components);
+
+        let res =
+            std::fs::write(&out_file, text).context(format!("Failed to write to {out_file:?}"));
+        if res.is_err() {
+            bail!("Encountered: {res:?}!");
+        }
+        Ok(())
+    } else {
+        bail!("Could not convert the file {file:?} to obsidian: {res:?}")
+    }
 }
 
 fn files_in_tree<T: AsRef<Path>>(root_dir: T) -> Result<Vec<PathBuf>> {
@@ -90,7 +156,10 @@ fn files_in_tree<T: AsRef<Path>>(root_dir: T) -> Result<Vec<PathBuf>> {
         .into_iter()
         .map(|f| {
             let path = f.unwrap().path();
-            if let Some(ext) = path.extension() {
+            if path.is_dir() {
+                let rec = files_in_tree(&path)?;
+                res.extend(rec.into_iter());
+            } else if let Some(ext) = path.extension() {
                 if ["md"].contains(&ext.to_str().unwrap_or("should not be found")) {
                     res.push(path.clone());
                 }
